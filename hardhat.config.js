806fc1631a889254b69f6a76561e06372211cd8d
Phillip D Shields <phill@noreply>
2021-11-01 14:52:35 +1300

    create-items page functioning properly

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,19 @@
+require('dotenv').config()
 require("@nomiclabs/hardhat-waffle");
 
-// file service
-const fs = require("fs")
 
-const privateKey = fs.readFileSync(".secret").toString();
-// change to env variable before production build
-const projectId = "5ad8fad3d452486d97c5799773a57925"
 
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337
     },
-    mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts: []
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PROJECT_SECRET}`]
     },
     mainnet: {
-      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: []
     }
   },
