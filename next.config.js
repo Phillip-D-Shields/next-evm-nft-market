0848254c063dbfabf638f5daa25a6381989c9d6f
phillip shields <matua.phillip.shields@gmail.com>
2022-05-22 21:10:42 +1200

    build bug fix test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-  target: "serverless"
+  target: "serverless",
+  webpack: (config, { webpack }) => {
+    config.plugins.push(new webpack.IgnorePlugin({
+        resourceRegExp: /^electron$/
+    }),);
+    return config
+}
 }
